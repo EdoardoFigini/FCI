005e241a1b39349c3a8eb1abcd7ed74c428ccaa0
EdoardoFigini <edoardoeffe4@gmail.com>
2022-03-14 15:27:41 -0400

    Lab FCI

--- 03_14/esercizio2_server.py
+++ 03_14/esercizio2_server.py
@@ -10,14 +10,14 @@ def main():
     while True:
         msg, clientAddress = serverSocket.recvfrom(2048)
         print(f'Received connection form {clientAddress[0]}:{clientAddress[1]}')
-        msg = msg.decode('utf-8')
+        n = int(msg.decode('utf-8'))
         
-        if (int(msg) <= 2):
+        if (n <= 2):
             return
 
         primo=True
-        for i in range(2, int(msg)):
-            if(int(msg)%i==0):
+        for i in range(2, n):
+            if(n%i==0):
                 primo = False
                 break
 
